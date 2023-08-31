5f5d7d6ed4c8d4c8ec934d6d3eee20b191f9dc73
Death-Knight-552 <discrilkx379@gmail.com>
2023-08-31 09:17:48 +0545

    major change

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,85 @@
 import Image from 'next/image'
 
+
+const books = [
+  {
+    name: "108 Maidens of Destiny",
+    author: "She's Cold as Ice",
+    translators: [
+      {
+        name: "The Sun Is Cold Translations",
+        home: "https://thesuniscold.translatednovels.com/",
+      },
+      {
+        name: "Amery Edge Teasers Translations",
+        home: "https://ameryedge.com/",
+      },
+    ],
+    coverImagePath:
+      "https://cdn.novelupdates.com/images/2016/03/108-bigger.png",
+    filePath:
+      "/files/108 Maidens of Destiny - 她酷像冰 (She’s Cold As Ice).epub",
+    plot: `“Your servant is Majestic Star ‘Panther Head’ Lin Chong, from this day forward I am your woman!”\n
+When the beautiful and heroic young girl in front of him shouted those heaven shattering words, Su Xing was overwhelmed with shock.\n
+And so the tale of the 108 maidens of destiny from legends begins.\n
+In Liangshan Continent, a genius is defined as a Star Master who is capable of signing the Star Duel Covenant contract with two Star Maidens. This was truly the limit.\n
+But right now Su Xing’s dilemma is how to convince the “Strength Star” Lu Junyi to become his eighth Star Knight. And this is just the beginning…`,
+  },
+];
+
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
+      <div id="cardholder">
+        {books.map((book, id) => {
+          return (
+            <div
+              id="card"
+              key={id}
+              className="p-4 border rounded-lg shadow-md w-96 text-center"
+            >
+              <h2 className="text-3xl font-semibold">{book.name}</h2>
+              <p className="text-gray-600">Author: {book.author}</p>
+              <div className="mt-4 w-full">
+                <Image
+                  unoptimized
+                  width={192}
+                  height={256}
+                  src={book.coverImagePath}
+                  alt={book.name}
+                  className="w-48 h-64 object-cover mx-auto rounded-lg"
+                />
+              </div>
+              <p className="mt-4 whitespace-pre-line text-justify">
+                {book.plot}
+              </p>
+              <div className="mt-4">
+                <p className="font-semibold">Translators:</p>
+                {book.translators.map((translator, id) => (
+                  <div key={id} className="mt-2">
+                    <a
+                      href={translator.home}
+                      className="text-blue-500 hover:underline"
+                    >
+                      {translator.name}
+                    </a>
+                  </div>
+                ))}
+              </div>
+              <div>
+                <a
+                  href={book.filePath}
+                  download
+                  className="bg-blue-500 mt-2 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+                >
+                  Download Book
+                </a>
+              </div>
+            </div>
+          );
+        })}
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+        
     </main>
   )
 }
